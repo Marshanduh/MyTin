5179d0e00ee95b1ad3971a93c59f9fec76819419
Sophia <121834765+slentsy@users.noreply.github.com>
2024-06-07 12:32:53 +0700

    macOS View

---
+++ MacMyTin/Views/Homepage.swift
@@ -0,0 +1,18 @@
+//
+//  Homepage.swift
+//  MacMyTin
+//
+//  Created by Sophia Madlentsy Tambunan on 6/7/24.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Homepage()
+}
---
+++ MacMyTin/Views/LaunchApp.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchApp.swift
+//  MacMyTin
+//
+//  Created by Sophia Madlentsy Tambunan on 6/7/24.
+//
+
+import SwiftUI
+
+struct LaunchApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchApp()
+}
