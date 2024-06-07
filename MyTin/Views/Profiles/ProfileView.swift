c9dd5cb42231e562c912ba1dd903a9e4a7d13aa2
owensproject <owentan0505@gmail.com>
2024-06-07 20:29:59 +0700

    Create ProfileView.swift
    Punya nya prayogo tp gtw kenapa hilang pas aku push

---
+++ MyTin/Views/Profiles/ProfileView.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileView.swift
+//  MyTin
+//
+//  Created by MacBook Pro on 25/05/24.
+//
+import SwiftUI
+
+struct ProfileView: View {
+    @State private var profile = User(name: "owenn", emailAddress: "owen@gmail.com", phoneNumber: "08112345678987", picture: "Satoru Gojo")
+    @State private var isEditing = false
+    
+    var body: some View {
+        VStack {
+            if isEditing {
+                ProfileEdit(profile: $profile, isEditing: $isEditing)
+                    .navigationBarBackButtonHidden(true)
+            } else {
+                Profile(user: profile)
+            }
+        }
+        .navigationTitle("Profile")
+        .navigationBarItems(
+            trailing: Button(action: {
+                isEditing.toggle()
+            }){
+                Text(isEditing ? "Done" : "Edit")
+            }
+        )
+    }
+}
+
+#Preview{
+    ProfileView()
+}
+//Test
